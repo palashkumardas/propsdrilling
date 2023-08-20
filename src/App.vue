9dff65323b69ce3c09ffc537e965ba24165d12d1
palashkumardas <info.palash27@gmail.com>
2023-08-20 18:32:30 +0600

    start props drilling

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,12 @@
 <script setup>
 import { ref,reactive } from 'vue';
-import ButtonComponent from './components/ButtonComponent.vue';
-import SlotComponent from './components/SlotComponent.vue';
-const message=ref("Hello World");
 
-function clickHandlerOne(){
-    message.value="Click One";
-}
-function clickHandlerTwo(){
-    message.value="Click Two";
-}
+
 </script>
 
 <template>
-  <h2 class="text-2xl my-10">Slot</h2>
-    <p class="my-10">{{ message }}</p>
-    <ButtonComponent :clickHandler="clickHandlerOne">
-        <em class="text-white-400">Button One</em>
-    </ButtonComponent>
-
-    <ButtonComponent :clickHandler="clickHandlerTwo">
-        <em class="text-white-400">Button Two</em>
-    </ButtonComponent>
-
-    <p>
-        <SlotComponent>
-            <template v-slot:header>This is header content</template>
-            <template v-slot:default>default</template>
-            <template v-slot:footer>This is footer content</template>
-        </SlotComponent>
-    </p>
+  <h2 class="text-2xl my-10">Props Drilling</h2>
+    
 </template>
 
 <style scoped>
