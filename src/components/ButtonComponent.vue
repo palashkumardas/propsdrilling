ff56934e965b25994f4654d737141e18c0350f13
palashkumardas <info.palash27@gmail.com>
2023-08-20 18:30:45 +0600

    end slot

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, reactive } from 'vue';
+defineProps(['clickHandler']);
+</script>
+
+<template>
+    <button @click="clickHandler" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1">
+    <slot></slot>
+
+    </button>
+ 
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SlotComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref,reactive } from 'vue';
+</script>
+
+<template>
+    <p><slot name="header"></slot></p>
+    <p><slot name="default">This is default content</slot></p>
+    <p><slot name="footer"></slot></p>
+
+</template>
+
+<style scoped>
+
+</style>
